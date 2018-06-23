8e460c7356dfa7d0788a4a3021a8748ae59b52bb
Sh0zam <koerich57@gmail.com>
2018-06-23 15:19:01 -0300

    Calculadora de Python Todos os Operadores

---
+++ calculadora.py
@@ -0,0 +1,42 @@
+##Meu primeiro Codigo de python 
+##By:Shazam
+##Calculadora de todas as operações 
+x = int(input("Primeiro Numero: "));
+operacao = input("Operação: ");
+y = int(input("Segundo Numero: "));
+
+	
+if operacao == "+":
+
+
+	print("Seu Resultado é:", x + y);
+
+
+elif operacao == "-":
+
+
+	print("Seu Resultado é:", x - y)
+
+
+elif operacao == "*":
+
+
+	print ("Seu Resultado é:", x * y)
+
+
+elif operacao == "/":
+
+
+	print(" Seu resultado é:", x / y )
+
+
+else:
+
+
+	print('A operação é invalida')
+
+
+print ("Codigo BY:Shazam");
+
+
+
