2dac96a8a7785e39e2427a554a9622c33384f6e0
Shazam <40308938+Sh0zam@users.noreply.github.com>
2018-07-09 20:32:35 -0300

    Calculadora Atulizada!

---
+++ Primeiros Scripts Em Python 3.7/calculadora.py
@@ -0,0 +1,102 @@
+##Meu primeiro Codigo de python 
+##By:Shazam
+##Calculadora de todas as operações 
+def menu():
+	 x = float(input("Primeiro Numero: "));
+
+	 
+	 print("")
+	 
+	 operacao = input("Operação: ");
+
+
+	 print("")
+
+	 y = float(input("Segundo Numero: "));
+
+
+	 print("")
+
+	
+	 if operacao == "+":
+
+
+		 print("Seu Resultado é:", x + y)
+
+
+		 print("")
+
+
+	 elif operacao == "-":
+
+
+		 print("Seu Resultado é:", x - y)
+
+
+		 print("")
+
+
+	 elif operacao == "*":
+
+
+		 print ("Seu Resultado é:", x * y)
+
+
+		 print("")
+
+
+	 elif operacao == "/":
+
+
+		 print(" Seu resultado é:", x / y )
+
+
+		 print("")
+
+
+	 else:
+
+
+		 print('A operação é invalida')
+
+
+		 print("")
+
+     
+     
+
+	 print ("Codigo BY:Shazam");
+
+
+	 print("");
+
+
+	 print("");
+
+
+	 x = int(input("""Você quer voltar para o menu digite: 0
+Se você não quer continuar usando aperte qualquer numero: """));
+
+
+	 print("");
+
+
+	 print("");
+
+
+	 if x == 0:
+	 	menu()
+
+
+	 else:
+	    print("Obrigado por usar meu programa !");
+	    print("Version 0.2.1");
+
+
+menu();
+
+##Use e melhore esse codigo 
+##Qualquer um pode usar !
+
+
+    
